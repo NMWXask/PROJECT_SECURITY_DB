f53260a4ef66227cda33352032cd4b3df76eb453
NMWXask <k.marcus@mail.ru>
2024-02-15 19:00:13 +0400

    Correct work with password 123 Spring Security

--- src/main/java/com/xask/ProjectSecurityDbApplication.java
+++ src/main/java/com/xask/ProjectSecurityDbApplication.java
@@ -1,11 +1,13 @@
 package com.xask;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ProjectSecurityDbApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectSecurityDbApplication.class);
+
 	}
 }
 
--- src/main/java/com/xask/config/SecurityConfiguration.java
+++ src/main/java/com/xask/config/SecurityConfiguration.java
@@ -1,14 +1,24 @@
 package com.xask.config;
 
+import com.xask.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
 
     @Bean
@@ -27,4 +37,5 @@ public class SecurityConfiguration {
         return http.build();
 
     }
+
 }
